b4b44610e74e8dcdce372bfa8d036b87ce816cb4
Salvatore Cahyo <salvacmp18@gmail.com>
2024-10-07 09:51:51 +0200

    Update tin_extruder_esp.ino

--- tin_extruder_esp/tin_extruder_esp.ino
+++ tin_extruder_esp/tin_extruder_esp.ino
@@ -1,5 +1,6 @@
 #include <WiFi.h>
-
+#define STEP 27
+#define DIR 26
 // Replace with your network credentials
 const char *ssid = "VKA5Y3-M1013S";
 const char *password = "homberger";
@@ -13,7 +14,8 @@ void setup() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-
+  pinMode(STEP, OUTPUT);
+  pinMode(DIR, OUTPUT);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
@@ -21,27 +23,36 @@ void setup() {
 
   Serial.println("Connected to WiFi!");
   Serial.println(WiFi.localIP());
-
+  digitalWrite(DIR, 1);
   // Start the server
   server.begin();
 }
 
 void loop() {
-  // Check if a client has connected
-  WiFiClient client = server.available();
+
+  // delay(1000);
+  //Check if a client has connected
+    WiFiClient client = server.available();
   if (client) {
     Serial.println("New Client Connected");
 
     while (client.connected()) {
       if (client.available()) {
         String command = client.readStringUntil('\n');
+        command.trim();
         Serial.print("Received command: ");
         Serial.println(command);
 
         // Process the command
         if (command == "EXTRUDE") {
           // Do something, e.g., turn on an LED
-          Serial.println("Turning ON");
+          Serial.println("Spinning");
+          client.println("ACK");
+          for (int i = 0; i < 100; i++) {
+            Serial.println(digitalRead(STEP));
+            digitalWrite(STEP, !digitalRead(STEP));
+            delay(10);
+          }
         } else {
           Serial.println("Unknown Command");
         }
