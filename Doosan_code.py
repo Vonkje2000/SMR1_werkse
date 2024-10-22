fe6122c508b38e2bbb0562126ea504a88135064e
Vonkje2000 <75570348+Vonkje2000@users.noreply.github.com>
2024-10-22 11:38:19 +0200

    Update Doosan_code.py
    added all the changes of the last two weeks to the code

--- Doosan_code.py
+++ Doosan_code.py
@@ -17,15 +17,20 @@ def get_to_point_by_angle(_pos_x, _pos_y, _pos_z, _Angle, _distance, _s_wait_tim
     #position to the side
     movel(add_pose(posx(_pos_x + x_offset, _pos_y + y_offset, _pos_z + _distance, 0, 0, 0), angleToAA(_Angle)), vel=velocity, acc=accelleration)
 
+    async_send_extrude_command(100)   #80
     #final position
     if(force_feedback):
         move_until_feedback(add_pose(posx(_pos_x           , _pos_y           , _pos_z            , 0, 0, 0), angleToAA(_Angle)))
     else:
         movel(add_pose(posx(_pos_x           , _pos_y           , _pos_z            , 0, 0, 0), angleToAA(_Angle)), vel=30, acc=30)
     
-    send_extrude_command(100)
-    wait(_s_wait_time)
-    send_extrude_command(-100)
+    wait(_s_wait_time / 5 * 1)
+    send_extrude_command(100)   #80
+    wait(_s_wait_time / 5 * 1)
+    send_extrude_command(100)   #80
+    wait(_s_wait_time / 5 * 1)
+    wait(_s_wait_time / 5 * 1)
+    wait(_s_wait_time / 5 * 1)
 
     #back to position on the side
     movel(add_pose(posx(_pos_x + x_offset, _pos_y + y_offset, _pos_z + _distance, 0, 0, 0), angleToAA(_Angle)), vel=velocity, acc=accelleration)
@@ -148,6 +153,19 @@ def send_extrude_command(steps=10):
 
     return 0
 
+### function to send an amount of steps that the stepper on the ESP32 can rotate without waithing until it is done ###
+def async_send_extrude_command(steps):
+    port = 4242
+    ip = "192.168.137.40"
+
+    socket = client_socket_open(ip, port)
+
+    client_socket_write(socket, (str(steps) + "\r\n").encode())
+
+    client_socket_close(socket)
+
+    return 0
+
 ### function to add a vector to a XY position ###
 def add_vector_to_pos_xy(_pos, angle, distance):
     _pos[1] = _pos[1] + sin(d2r(_pos[2] + angle)) * distance
@@ -169,9 +187,14 @@ def soldeer():
     if _mold_number == 0:
         _pos_x += 0
         _pos_y += 0
-        _pos_r += -0.8
+        _pos_r += -1.5           # - degrees is rotating clockwise, + degrees is rotating anti clockwise
 
     if _mold_number == 1:
+        _pos_x += 0
+        _pos_y += 0
+        _pos_r += 0
+
+    if _mold_number == 2:
         _pos_x += 0
         _pos_y += 0
         _pos_r += 0
@@ -180,8 +203,14 @@ def soldeer():
     #tp_log("number: " + str(_mold_number))
     #tp_log("offset: " + str(offset))
 
-    _pos = add_vector_to_pos_xy(_pos, 270, 9)   # distance from data matrix to the aluminium
-    _pos = add_vector_to_pos_xy(_pos, 180, 10)  # distance from data matrix 90 degrees of to the aluminium
+    if _mold_number == 0 or _mold_number == 1:
+        _pos = add_vector_to_pos_xy(_pos, 270, 9)  # distance from data matrix to the aluminium
+        _pos = add_vector_to_pos_xy(_pos, 180, 13)  # distance from data matrix 90 degrees of to the aluminium
+    elif _mold_number == 2:
+        _pos = add_vector_to_pos_xy(_pos, 270, -49)  # distance from data matrix to the aluminium needs to be negative. the metrix is at the other side
+        _pos = add_vector_to_pos_xy(_pos, 180, 15)  # distance from data matrix 90 degrees of to the aluminium
+    else:
+        return 0
 
     _offset_pos = add_vector_to_pos_xy([0,0,_pos_r], 0, offset)
     _offset_pos_inverse = add_vector_to_pos_xy([0, 0, _pos_r], 180, offset)
@@ -211,31 +240,31 @@ def soldeer():
 
     _z_offset = (_pos_2[2] - _pos_1[2])/8
 
-    get_to_point_by_angle(_pos[0] + _offset_pos[0], _pos[1] + _offset_pos[1], _pos_1[2] + _z_offset*0, _pos[2] + 180, 10, 2, True)
+    get_to_point_by_angle(_pos[0] + _offset_pos[0], _pos[1] + _offset_pos[1], _pos_1[2] + _z_offset*0, _pos[2] + 180, 10, 20, True)
 
     for i in range(8):
         _pos = add_vector_to_pos_xy(_pos, 0, 20)
-        get_to_point_by_angle(_pos[0] + _offset_pos[0], _pos[1] + _offset_pos[1], _pos_1[2] + _z_offset*i, _pos[2] + 180, 10, 2, True)
+        get_to_point_by_angle(_pos[0] + _offset_pos[0], _pos[1] + _offset_pos[1], _pos_1[2] + _z_offset*i, _pos[2] + 180, 10, 20, True)
         
-
-    _pos = add_vector_to_pos_xy(_pos, 0, 3)
-    get_to_point_by_angle(_pos[0] + _offset_pos_inverse[0], _pos[1] + _offset_pos_inverse[1], _pos_1[2] + _z_offset*8, _pos[2], 10, 2, True)
+    _pos = add_vector_to_pos_xy(_pos, 90, 4)  # distance from data matrix to the aluminium
+    _pos = add_vector_to_pos_xy(_pos, 0, 4)
+    get_to_point_by_angle(_pos[0] + _offset_pos_inverse[0], _pos[1] + _offset_pos_inverse[1], _pos_1[2] + _z_offset*8, _pos[2], 10, 20, True)
 
     for i in range(8):
         _pos = add_vector_to_pos_xy(_pos, 180, 20)
-        get_to_point_by_angle(_pos[0] + _offset_pos_inverse[0], _pos[1] + _offset_pos_inverse[1], _pos_1[2] + _z_offset*(8-i), _pos[2], 10, 2, True)
+        get_to_point_by_angle(_pos[0] + _offset_pos_inverse[0], _pos[1] + _offset_pos_inverse[1], _pos_1[2] + _z_offset*(8-i), _pos[2], 10, 20, True)
     return 0
 
 ### Function to measure and calculate the offset of the tip of the solder iron from the perfect centre of the flench ###
 def calculate_offset_center(_posx):
-    _posx[2] = 95
+    _posx[2] = 145
     movel(_posx, acc=accelleration, vel=velocity)
     _posx[2] = 80
     move_until_feedback(_posx)
-    z1 = 1.825575256347656 + 80.89949 # pre determant value in mm       # 1.825575256347656 is the value of hitting the table
+    z1 = 1.825575256347656 + 108.974 # pre determant value in mm       # 1.825575256347656 is the value of hitting the table
     _pos, _i = get_current_posx() # pose in which the robot stops and take z value 
     z2 = _pos[2]
-    offset = (z1-z2)/tan(45/180*3.14) # calculate offset trough formula
+    offset = (z1-z2)/tan(45/180*3.14) + 3.70496 # calculate offset trough formula
     return offset, z2
 
 ### sometimes the robot is not that accurate so this function calculates the average offset ###
@@ -247,9 +276,13 @@ def calculate_average_offset_center(_posx):
         _offsets.append(_o)
         _z_heights.append(_z)
     _offsets.sort()
-    z2 = sum(_offsets[1:-1]) / (len(_offsets) - 2)
+    offset = sum(_offsets[1:-1]) / (len(_offsets) - 2)
     _z_heights.sort()
-    offset = sum(_z_heights[1:-1]) / (len(_z_heights) - 2)
+    z2 = sum(_z_heights[1:-1]) / (len(_z_heights) - 2)
+    #z2 += 8        # only for the solder iron with the flat side down
+    #offset -= 4
+    z2 += 4        # only for the solder iron with the flat side
+    offset -= 2
     return offset, z2
 
 ### function that can be called to test some lines of code ###
@@ -277,6 +310,7 @@ Z_10_CM_up = posx(0, 0, 100, 0, 0, 0)
 
 accelleration = 100
 velocity = 100
+offset_tolerance = 1
 # end of defined variables
 
 # start of the code
@@ -291,12 +325,26 @@ movel(boot_up_pos, vel=velocity, acc=accelleration)
 move_home(DR_HOME_TARGET_USER)
 
 tp_popup('Lookout robot arm starts calibrating.', pm_type=DR_PM_MESSAGE, button_type=1)
-z_height, offset = calculate_average_offset_center(get_current_posx()[0])
+offset, z_height = calculate_average_offset_center(get_current_posx()[0])
 #tp_log("offset : " + str(offset) + " , z height : " + str(z_height))
 move_home(DR_HOME_TARGET_USER)
 
+one_time_flag = True
 while True:
     tp_popup('Start new soldering job?', pm_type=DR_PM_MESSAGE, button_type=0)
+    if(one_time_flag):
+        one_time_flag = False
+    else:
+        tp_popup('Lookout robot arm checks calibration.', pm_type=DR_PM_MESSAGE, button_type=1)
+        _check_offset, _check_z_height = calculate_offset_center(get_current_posx()[0])
+        move_home(DR_HOME_TARGET_USER)
+        tp_log(str(abs(offset - _check_offset) > offset_tolerance))
+        if(abs(offset - _check_offset) > offset_tolerance):
+            tp_popup('Needs to recalibrate! Lookout robot arm starts calibrating.', pm_type=DR_PM_MESSAGE, button_type=1)
+            offset, z_height = calculate_average_offset_center(get_current_posx()[0])
+            move_home(DR_HOME_TARGET_USER)
+            tp_popup('Continue soldering job?', pm_type=DR_PM_MESSAGE, button_type=0)
+
     #test()
     #function_test()
     soldeer()
